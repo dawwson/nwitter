0ea87a274e6f70664d0db450fd3fd1b4ecc6e567
dawwson <shines1427@gmail.com>
2024-09-09 17:26:44 +0900

    Feat: 라우팅 & Loading 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,71 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+
 function App() {
-  return <></>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const init = async () => {
+    // firebase 기다림
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
 }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  // Layout으로 감싸지 않는 경로
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      {/* TODO: Navigation bar */}
+      <h2>Layout!</h2>
+      <Outlet />
+    </>
+  );
+}
