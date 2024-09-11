95e4aed27d1d23139bd699c45ae40b54f26b92cc
dawwson <shines1427@gmail.com>
2024-09-11 15:54:59 +0900

    Feat: 실시간 트윗 업데이트 기능 추가

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
 import * as S from "./style";
 import { db } from "../../configs/firebase";
 import Tweet from "../tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface TweetDocument {
   id: string;
@@ -16,28 +23,41 @@ export interface TweetDocument {
 
 const Timeline = () => {
   const [tweets, setTweets] = useState<TweetDocument[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets: TweetDocument[] = snapshot.docs.map((doc) => {
-      const { tweet, userId, username, downloadUrl, createdAt } = doc.data();
-      return {
-        id: doc.id,
-        tweet,
-        userId,
-        username,
-        downloadUrl,
-        createdAt,
-      };
-    });
-    setTweets(tweets);
-  };
 
   useEffect(() => {
+    let unsubscribe: Unsubscribe;
+
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+
+      // NOTE: query에 이벤트 리스너를 등록하여 업데이트된 collection을 실시간으로 가져온다.
+      unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets: TweetDocument[] = snapshot.docs.map((doc) => {
+          const { tweet, userId, username, downloadUrl, createdAt } =
+            doc.data();
+
+          return {
+            id: doc.id,
+            tweet,
+            userId,
+            username,
+            downloadUrl,
+            createdAt,
+          };
+        });
+
+        setTweets(tweets);
+      });
+    };
+
     fetchTweets();
+
+    // cleanup : 이벤트 리스너 구독 취소
+    return () => unsubscribe && unsubscribe();
   }, []);
 
   return (
